59b75a684170cf119ac218f402aca25ba463b97d
GarbageCan622 <89987964+GarbageCan622@users.noreply.github.com>
2023-03-08 14:22:34 -0500

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,1 +1,12 @@
-
+require("dotenv").config();
+const {Client, GatewayIntentBits} = require("discord.js");
+const client = new Client({
+	intents:[
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages
+	]
+});
+client.once("ready", () =>{
+	console.log("BOT IS ONLINE");
+})
+client.login(process.env.DISCORD_TOKEN);
