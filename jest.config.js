91013d1d233949dfaaf414080199b16227e42f3a
GarbageCan622 <89987964+GarbageCan622@users.noreply.github.com>
2023-03-14 16:37:18 -0400

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-const TEST_REGEX = "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$";
-
-module.exports = {
-    preset: 'js-jest',  // use ts-jest if using typescript
-    testEnvironment: 'node',
-    transformIgnorePatterns: ['^.+\\.js$'],
-}
