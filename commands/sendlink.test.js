3e9c7b7e4ebf0409ab721871ecce3cc185011d1f
GarbageCan622 <89987964+GarbageCan622@users.noreply.github.com>
2023-03-14 16:46:47 -0400

    Delete sendlink.test.js

--- commands/sendlink.test.js
+++
@@ -1,7 +0,0 @@
-const linkMessage = require("./sendlink");
-
-describe("Send Link Command", () => {
-	test('link is correct', () => {
-		expect(linkMessage.site).toBe('https://garbagecan622.github.io/Schedgy/');
-	});
-});
