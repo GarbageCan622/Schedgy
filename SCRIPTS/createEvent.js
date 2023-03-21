885253d6d54dcbbdf29879c61dbece874d5c7136
CliftonLandry <113145925+CliftonLandry@users.noreply.github.com>
2023-03-21 17:27:40 -0400

    Added calendar to sign up page

--- SCRIPTS/createEvent.js
+++ SCRIPTS/createEvent.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
     console.log("boop");
     $('input[name="daterange"]').daterangepicker({
-        alwaysShowCalendars: true,
-        opens: 'left'
+        "alwaysShowCalendars": true,
+        "opens": 'left',
+        startDate: moment(),
+        endDate: moment().add(1, 'day')
       }, function(start, end, label) {
         console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
       });
     
-    $( "#datePick" ).click(function() {
+      $("#DateTypes").change(function(){     
+    if($("#DateTypes option:selected").val() == 'SpecificDates'){
+        $('#SpecificDates').css('display', 'block');
+        $('#DaysOfTheWeek').css('display', 'none');
+    }
+    else{
+        $('#SpecificDates').css('display', 'none');
+        $('#DaysOfTheWeek').css('display', 'block');
+    }
     });
 });
