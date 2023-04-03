be13e91aa8e2998c81cdb6778fec76e7321c3e63
Jayendra Rathore <71295474+JayRathore02@users.noreply.github.com>
2023-04-03 14:44:36 -0400

    Edited the the test.bot.js
    Still doesn't work but I'm just doing some more testing

--- bot.test.js
+++ bot.test.js
@@ -1,3 +1,31 @@
-const { MockInteraction } = require('discord.js-mock-interactions');
-import ('/Users/jayendrarathore/Documents/GitHub/Schedgy/bot.js')
+const { DiscordMock } = require('discord.js-mock-interactions');
+const { bot } = require('bot.js')
 
+describe('Ping command', () => {
+    test('Bot should respond with "Pong"', async () => {
+        const mock = new DiscordMock();
+        const interaction = mock.createInteraction({name: 'ping', type: 1});
+
+        await mock.simulate(interaction);
+
+        const response = await bot(interaction);
+        expect(interaction.reply).toEqual('Pong');
+    })
+
+});
+
+/*
+const { MockInteraction } = require('discord.js-mock-interactions');
+const bot = require('bot.js');
+const { commands } = bot;
+
+describe('Ping command', () => {
+    it('should reply with "Pong"', async () => {
+        const interaction = new MockInteraction();
+        await commands.get('ping').execute(interaction);
+
+        expect(interaction.replied).toBe(true);
+        expect(interaction.reply).toBe('Pong');
+    });
+});
+*/
