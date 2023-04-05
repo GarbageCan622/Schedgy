3ffdc1aefe864289316027ff5eb559cdceba34af
Jayendra Rathore <71295474+JayRathore02@users.noreply.github.com>
2023-04-05 18:49:44 -0400

    Testing in the commands folder
    Experimenting some new ways to test.

---
+++ commands/ping.test.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+const ping = require('./ping');
+
+describe('Ping Command', () => {
+    test('Reply with "Pong"', async () => {
+        const mockInteraction = {
+            reply: jest.fn(),
+        }
+        await ping.execute(mockInteraction);
+        expect(mockInteraction.reply).toHaveBeenCalledWith('Pong');
+    })
+})
