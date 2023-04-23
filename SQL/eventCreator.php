81bc72241411fd43a95dd9a6dada54a5c0921b5b
saltyseas69 <116859227+saltyseas69@users.noreply.github.com>
2023-04-23 13:13:59 -0400

    Update eventCreator.php

--- SQL/eventCreator.php
+++ SQL/eventCreator.php
@@ -52,8 +52,122 @@
                             <div id="WhatDates" style="font-family:discord; color:white; font-size:18px; text-align: center; width: 50%; min-width:305px;">
                                 What day of the week might work?
                             </div>    
+                            <div style="font-family:discord; color:white; font-size:15px;line-height:1.5; margin: 10px 0px 10px 0px;">
+                                Select Specific Dates.<br>
+                                Click a start and end date for your event.<br>
+                            </div>
+                            <div id='SpecificDates'>
+                                <input type="text" id="datePick" name="daterange" readonly value="03/17/2023 - 03/23/2023"/>
+                            </div>
+                        </td>
+
+                        <td align=center valign=top rowspan=2 width="50%">
+                            <div id="WhatTimes" style="font-family:discord; color:white;font-size:20px;text-align: center; width: 50%; min-width:305px;">
+                                What times might work?
+                            </div>
+                            <div style="font-family:discord; color:white;font-size:15px;line-height:1.5; margin: 10px 0px 10px 0px;">
+                                No earlier than: 
+                                <select name="NoEarlierThan" id="NoEarlierThan">
+                                    <option value=0>12:00  AM</option>  
+                                    <option value=1>1:00  AM</option>  
+                                    <option value=2>2:00  AM</option>  
+                                    <option value=3>3:00  AM</option>  
+                                    <option value=4>4:00  AM</option>  
+                                    <option value=5>5:00  AM</option>  
+                                    <option value=6>6:00  AM</option>  
+                                    <option value=7>7:00  AM</option>  
+                                    <option value=8>8:00  AM</option>  
+                                    <option selected value=9>9:00  AM</option>  
+                                    <option value=10>10:00  AM</option>  
+                                    <option value=11>11:00  AM</option>  
+                                    <option value=12>12:00  PM</option>  
+                                    <option value=13>1:00  PM</option>  
+                                    <option value=14>2:00  PM</option>  
+                                    <option value=15>3:00  PM</option>  
+                                    <option value=16>4:00  PM</option>  
+                                    <option value=17>5:00  PM</option>  
+                                    <option value=18>6:00  PM</option>  
+                                    <option value=19>7:00  PM</option>  
+                                    <option value=20>8:00  PM</option>  
+                                    <option value=21>9:00  PM</option>  
+                                    <option value=22>10:00  PM</option>  
+                                    <option value=23>11:00  PM</option>  
+                                    <option value=0>12:00  AM</option>
+                                </select>
+                            </div>
+                        
+                            <div style="font-family:discord; color:white;font-size:15px;line-height:1.5; margin: 10px 0px 10px 0px;">
+                                No later than: 
+                                <select name="NoLaterThan" id="NoLaterThan">
+                                    <option value=0>12:00  AM</option>  
+                                    <option value=1>1:00  AM</option>  
+                                    <option value=2>2:00  AM</option>  
+                                    <option value=3>3:00  AM</option>  
+                                    <option value=4>4:00  AM</option>  
+                                    <option value=5>5:00  AM</option>  
+                                    <option value=6>6:00  AM</option>  
+                                    <option value=7>7:00  AM</option>  
+                                    <option value=8>8:00  AM</option>  
+                                    <option value=9>9:00  AM</option>  
+                                    <option value=10>10:00  AM</option>  
+                                    <option value=11>11:00  AM</option>  
+                                    <option value=12>12:00  PM</option>  
+                                    <option value=13>1:00  PM</option>  
+                                    <option value=14>2:00  PM</option>  
+                                    <option value=15>3:00  PM</option>  
+                                    <option value=16>4:00  PM</option>  
+                                    <option selected value=17>5:00  PM</option>  
+                                    <option value=18>6:00  PM</option>  
+                                    <option value=19>7:00  PM</option>  
+                                    <option value=20>8:00  PM</option>  
+                                    <option value=21>9:00  PM</option>  
+                                    <option value=22>10:00  PM</option>  
+                                    <option value=23>11:00  PM</option>  
+                                    <option value=0>12:00  AM</option>
+                                </select>
+                            </div>
                             <br>
-                            <label for="start_day" style="font-family:discord;color:white;font-size:20px;">Select Starting Day:</label>
+                            
+                                <input type=submit name="CreateEvent" value="Create Event" id="CreateEvent" style="font-size:12px;">
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+                    
+        <?php
+            $result="";
+            if(isset($_POST['CreateEvent'])) {
+                $id = $_POST['event_id'];
+                $eventname = $_POST['event_name'];
+                $description = $_POST['description'];
+                $star = $_POST['start'];
+                $end = $_POST['end'];
+            
+                if (empty($id) || empty($_SESSION['sessionID']) || empty($eventname) || empty($description) || empty($start) || empty($end)) {
+                    echo "Data required in all fields";
+                } else {
+                    $query = 'insert into event (event_id, owner_id, event_name, description, start, end) values (' . $id . ',' .$_SESSION['sessionID']. ',"' .$eventname. '","' .$description. '","' .$start. '","' .$end. '")';                    
+                    $result = mysqli_query($dbConnection, $query);
+                    $createauthor = 'insert into owner_of values (' .$id. ',"' .$_SESSION['sessionID'].'")';
+                    $author_result = mysqli_query($dbConnection, $createauthor);
+                }
+                    if (!$result) {
+                        echo "<br>Could not create new event!<br>";
+                    } else {
+                        echo "<br>Event succsefully created!<br>";
+                    }
+                }
+
+        ?>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+        <script type="text/javascript" src="../SCRIPTS/createEvent.js" ></script>
+        
+        <!-- <label for="start_day" style="font-family:discord;color:white;font-size:20px;">Select Starting Day:</label>
                                 <select name="start_day">
                                     <option value=Sunday>Sunday</option>  
                                     <option value=Monday>Monday</option>  
@@ -73,15 +187,8 @@
                                     <option value=Thursday>Thursday</option>  
                                     <option selected value=Friday>Friday</option>  
                                     <option value=Saturday>Saturday</option>     
-                                </select>
-                        </td>
-
-                        <td align=center valign=top rowspan=2 width="50%">
-                            <div id="WhatTimes" style="font-family:discord; color:white;font-size:20px;text-align: center; width: 50%; min-width:305px;">
-                                What times might work?
-                            </div>
-                            <br>
-                            <label for="start_time" style="font-family:discord;color:white;font-size:20px;">No Earlier Than:</label>
+                                </select> -->
+                                <!-- <label for="start_time" style="font-family:discord;color:white;font-size:20px;">No Earlier Than:</label>
                                 <select name="start_time">
                                     <option value=00:00:00>12:00  AM</option>  
                                     <option value=01:00:00>1:00  AM</option>  
@@ -135,46 +242,6 @@
                                     <option value=21:00:00>9:00  PM</option>  
                                     <option value=22:00:00>10:00  PM</option>  
                                     <option value=23:00:00>11:00  PM</option>
-                                <br><br>
-                                <input type=submit name="CreateEvent" value="Create Event" id="CreateEvent" style="font-size:12px;">
-                            </form>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-                    
-        <?php
-            $result="";
-            if(isset($_POST['CreateEvent'])) {
-                $id = $_POST['event_id'];
-                $eventname = $_POST['event_name'];
-                $description = $_POST['description'];
-                $startday = $_POST['start_day'];
-                $endday = $_POST['end_day'];
-                $starttime = $_POST['start_time'];
-                $endtime = $_POST['end_time'];
-            
-                if (empty($id) || empty($_SESSION['sessionID']) || empty($eventname) || empty($description) || empty($startday) || empty($endday) || empty($starttime) || empty($endtime)) {
-                    echo "Data required in all fields";
-                } else {
-                    $query = 'insert into event (event_id, owner_id, event_name, description, start_day, end_day, start_time, end_time) values (' . $id . ',' .$_SESSION['sessionID']. ',"' .$eventname. '","' .$description. '","' .$startday. '","' .$endday. '","' .$starttime. '","' .$endtime. '")';                    
-                    $result = mysqli_query($dbConnection, $query);
-                    $createauthor = 'insert into owner_of values (' .$id. ',"' .$_SESSION['sessionID'].'")';
-                    $author_result = mysqli_query($dbConnection, $createauthor);
-                }
-                    if (!$result) {
-                        echo "<br>Could not create new event!<br>";
-                    } else {
-                        echo "<br>Event succsefully created!<br>";
-                    }
-                }
-
-        ?>
-        
-        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-        <script type="text/javascript" src="../SCRIPTS/createEvent.js" ></script> -->
+                                <br><br> -->
     </body>
 </html>
