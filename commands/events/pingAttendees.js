5ec2923589eb45832a84ff6e4398e9a86f7119e5
GarbageCan622 <89987964+GarbageCan622@users.noreply.github.com>
2023-04-24 18:42:13 -0400

    Update pingAttendees.js

--- commands/events/pingAttendees.js
+++ commands/events/pingAttendees.js
@@ -8,14 +8,22 @@ var con = mysql.createConnection({
   database: "s88864_Events"
 });
 
-getMembers = function(event_id) {
+
+getMembers = function(event_id, callerID) {
   return new Promise(function(resolve, reject){
-    let searchQuery = "SELECT guest_id FROM event, member_of WHERE event.event_id = member_of.event_id AND event.event_id = " + String(event_id)
+	// Check if user of command is the owner of the event
+    con.query("SELECT author_id FROM owner_of WHERE event_id = " + String(event_id),
+      function (err, rows) { 
+        if (rows[0].author_id != callerID) {
+          return reject(new Error("You do not own the event associated with ID: " + String(event_id)));
+        }
+    })
+    let searchQuery = "SELECT uid FROM event, member_of, users WHERE event.event_id = member_of.event_id AND event.event_id = " + String(event_id) + " AND guest_id = uid";
     con.query(
       searchQuery, 
       function (err, rows) { 
-        if(rows === undefined){
-          reject(new Error("Error rows is undefined"));
+        if(rows === undefined || rows.length == 0){
+          return reject(new Error("Event has no members"));
         } else {
           resolve(rows);
         }
@@ -23,32 +31,36 @@ getMembers = function(event_id) {
   )}
 )}
 
-/**
-getMembers(2)
-.then(function(results){
-  render(results)
-})
-.catch(function(err){
-  console.log("Promise rejection error: "+err)
-})
-
-render = function(results){ 
-  for (var i in results) console.log(results[i]) 
+resultsToString = function(results){
+  let out = ""
+  for (var i in results) {
+    //console.log(results[i].uname);
+    out = out.concat("<@" + results[i].uid + "> ");
+  }
+  return out;
 }
-*/
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('pingattendees')
 		.setDescription('Ping all members of an event')
-    .addIntegerOption(option =>
+    .addStringOption(option =>
         option
           .setName('event_id')
           .setDescription('The number ID of your event.')
           .setRequired(true)
       ),
 	async execute(interaction) {
-    getMembers(interaction.options.getInteger('event_id'))
-		await interaction.reply(results);
+    let temp = "Error"
+    let callerID = interaction.user.id;
+    await getMembers(interaction.options.getString('event_id'), callerID)
+      .then(function(results){
+        temp = resultsToString(results)
+      })
+      .catch(function(err){ // Catch block for errors / rejects
+        temp = err;
+      });
+    interaction.reply(String(temp));
 	},
 };
