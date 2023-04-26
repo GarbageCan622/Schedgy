51f2636f8852ac4486926fcc6bdcbcf05b607852
Jayendra Rathore <71295474+JayRathore02@users.noreply.github.com>
2023-04-26 00:01:57 -0400

    Update createEvent.test.js

--- SCRIPTS/createEvent.test.js
+++ SCRIPTS/createEvent.test.js
@@ -1,72 +1,42 @@
-describe('createEvent', () => {
-    beforeEach(() => {
-      document.body.innerHTML = `
-        <form id="EventForm">
-          <input id="NewEventName" type="text" value="Test Event" />
-          <select id="NoEarlierThan">
-            <option value="0">12:00am</option>
-            <option value="1">1:00am</option>
-            <option value="2">2:00am</option>
-          </select>
-          <select id="NoLaterThan">
-            <option value="22">10:00pm</option>
-            <option value="23">11:00pm</option>
-          </select>
-          <select id="DateTypes">
-            <option value="SpecificDates">Specific Dates</option>
-            <option value="DaysOfTheWeek">Days of the Week</option>
-          </select>
-          <div id="SpecificDates">
-            <input type="text" name="daterange" value="03/17/2023 - 03/23/2023" />
-          </div>
-          <div id="DaysOfTheWeek">
-            <input type="checkbox" name="days" value="monday" /> Monday
-            <input type="checkbox" name="days" value="tuesday" /> Tuesday
-            <input type="checkbox" name="days" value="wednesday" /> Wednesday
-            <input type="checkbox" name="days" value="thursday" /> Thursday
-            <input type="checkbox" name="days" value="friday" /> Friday
-            <input type="checkbox" name="days" value="saturday" /> Saturday
-            <input type="checkbox" name="days" value="sunday" /> Sunday
-          </div>
-          <input id="event_id" type="hidden" value="1" />
-          <button type="submit" id="submitBtn">Submit</button>
-        </form>
-      `;
-    });
-  
-    test('selecting Specific Dates shows the correct div', () => {
-      const specificDatesDiv = document.getElementById('SpecificDates');
-      const daysOfWeekDiv = document.getElementById('DaysOfTheWeek');
-      const dateTypesSelect = document.getElementById('DateTypes');
-  
-      dateTypesSelect.value = 'SpecificDates';
-      dateTypesSelect.dispatchEvent(new Event('change'));
-  
-      expect(specificDatesDiv.style.display).toBe('block');
-      expect(daysOfWeekDiv.style.display).toBe('none');
-    });
-  
-    test('selecting Days of the Week shows the correct div', () => {
-      const specificDatesDiv = document.getElementById('SpecificDates');
-      const daysOfWeekDiv = document.getElementById('DaysOfTheWeek');
-      const dateTypesSelect = document.getElementById('DateTypes');
-  
-      dateTypesSelect.value = 'DaysOfTheWeek';
-      dateTypesSelect.dispatchEvent(new Event('change'));
-  
-      expect(specificDatesDiv.style.display).toBe('none');
-      expect(daysOfWeekDiv.style.display).toBe('block');
-    });
-  
-    test('selecting a No Earlier Than time disables appropriate No Later Than options', () => {
-      const noLaterThanSelect = document.getElementById('NoLaterThan');
-      const noEarlierThanSelect = document.getElementById('NoEarlierThan');
-  
-      noEarlierThanSelect.value = '1';
-      noEarlierThanSelect.dispatchEvent(new Event('change'));
-  
-      expect(noLaterThanSelect.children[0].disabled).toBe(true);
-      expect(noLaterThanSelect.children[1].disabled).toBe(false);
-      expect(noLaterThanSelect.children[2].disabled).toBe(false);
-    });
+// sendWebhook.test.js
+
+const sendWebhook = require('./sendWebhook');
+
+test('sendWebhook sends a request to the correct URL', () => {
+  const openSpy = jest.spyOn(XMLHttpRequest.prototype, 'open');
+  sendWebhook();
+  expect(openSpy).toHaveBeenCalledWith('POST', 'https://discord.com/api/webhooks/WEBHOOK_ID/WEBHOOK_TOKEN');
+  openSpy.mockRestore();
+});
+
+test('sendWebhook sets the correct request header', () => {
+  const setRequestHeaderSpy = jest.spyOn(XMLHttpRequest.prototype, 'setRequestHeader');
+  sendWebhook();
+  expect(setRequestHeaderSpy).toHaveBeenCalledWith('Content-type', 'application/json');
+  setRequestHeaderSpy.mockRestore();
+});
+
+test('sendWebhook sends the correct payload', () => {
+  const sendSpy = jest.spyOn(XMLHttpRequest.prototype, 'send');
+  sendWebhook();
+  expect(sendSpy).toHaveBeenCalledWith('{"username":"Schedgy","avatar_url":"","content":"A new calendar event has been created on the Schedgy website @everyone"}');
+  sendSpy.mockRestore();
+});
+
+test('sendWebhook logs an error message when the request fails', () => {
+  const errorSpy = jest.spyOn(console, 'log');
+  const mockRequest = {
+    open: jest.fn(),
+    setRequestHeader: jest.fn(),
+    send: jest.fn(),
+    addEventListener: (event, callback) => {
+      if (event === 'error') {
+        callback();
+      }
+    }
+  };
+  global.XMLHttpRequest = jest.fn(() => mockRequest);
+  sendWebhook();
+  expect(errorSpy).toHaveBeenCalledWith('Error sending webhook');
+  errorSpy.mockRestore();
 });
