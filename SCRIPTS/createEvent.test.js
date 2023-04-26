0fc50e1c3cc18b503f80bda29fb0c65a618bd5e4
Jayendra Rathore <71295474+JayRathore02@users.noreply.github.com>
2023-04-25 22:05:28 -0400

    Update createEvent.test.js

--- SCRIPTS/createEvent.test.js
+++ SCRIPTS/createEvent.test.js
@@ -1,31 +1,86 @@
 const createEvent = require('./createEvent')
 
-describe (createEvent, () => {
-    test('XMLHttpRequest function is defined', () => {
-        expect(newXMLHttpRequest).toBeDefined();
+describe('createEvent', () => {
+    test('should send a POST request to the correct webhook URL', () => {
+      // mock XMLHttpRequest
+      const mockRequest = jest.fn();
+      window.XMLHttpRequest = jest.fn(() => ({
+        open: jest.fn(),
+        setRequestHeader: jest.fn(),
+        send: mockRequest,
+      }));
+    
+      // mock process.env
+      process.env.WEBHOOK_ID = '12345';
+      process.env.WEBHOOK_TOKEN = 'abcde';
+    
+      // call the function
+      createEvent();
+    
+      // check that the request was sent to the correct URL
+      expect(window.XMLHttpRequest.mock.calls[0][0]).toEqual('POST');
+      expect(window.XMLHttpRequest.mock.calls[0][1]).toEqual('https://discord.com/api/webhooks/12345/abcde');
     });
-
-    test('Webhook URL is correctly formatted', () => {
-        expect(webhookURL).toMatch(/https:\/\/discord\.com\/api\/webhooks\/\w+\/\w+/);
+    
+    test('should set the correct request headers', () => {
+      // mock XMLHttpRequest
+      const mockSetRequestHeader = jest.fn();
+      window.XMLHttpRequest = jest.fn(() => ({
+        open: jest.fn(),
+        setRequestHeader: mockSetRequestHeader,
+        send: jest.fn(),
+      }));
+    
+      // call the function
+      createEvent();
+    
+      // check that the request headers are set correctly
+      expect(mockSetRequestHeader.mock.calls[0][0]).toEqual('Content-type');
+      expect(mockSetRequestHeader.mock.calls[0][1]).toEqual('application/json');
     });
-
-    test('Message params object is correctly formatted', () => {
-        const expectedParams = {
-          username: "Schedgy",
-          avatar_url: "",
-          content: "A new calendar event has been created on the Schedgy website @everyone"
-        };
-        expect(params).toEqual(expectedParams);
+    
+    test('should send the correct message in the request body', () => {
+      // mock XMLHttpRequest
+      let sentParams;
+      window.XMLHttpRequest = jest.fn(() => ({
+        open: jest.fn(),
+        setRequestHeader: jest.fn(),
+        send: (params) => { sentParams = params; },
+      }));
+    
+      // call the function
+      createEvent();
+    
+      // check that the message is sent correctly in the request body
+      expect(sentParams).toEqual(JSON.stringify({
+        username: 'Schedgy',
+        avatar_url: '',
+        content: 'A new calendar event has been created on the Schedgy website @everyone',
+      }));
     });
-
-    test('request.send is called with correct parameters', () => {
-        const sendSpy = jest.spyOn(request, 'send');
-        const expectedParams = JSON.stringify({
-          username: "Schedgy",
-          avatar_url: "",
-          content: "A new calendar event has been created on the Schedgy website @everyone"
-        });
-        expect(sendSpy).toHaveBeenCalledTimes(1);
-        expect(sendSpy).toHaveBeenCalledWith(expectedParams);
-     });
-})
+    
+    test('should log an error if the request fails', () => {
+      // mock XMLHttpRequest
+      window.XMLHttpRequest = jest.fn(() => ({
+        open: jest.fn(),
+        setRequestHeader: jest.fn(),
+        addEventListener: (eventName, callback) => {
+          if (eventName === 'load') {
+            callback();
+          } else if (eventName === 'error') {
+            callback();
+          }
+        },
+        send: jest.fn(),
+      }));
+    
+      // mock console.log
+      const mockLog = jest.spyOn(console, 'log');
+    
+      // call the function
+      createEvent();
+    
+      // check that an error is logged if the request fails
+      expect(mockLog).toHaveBeenCalledWith('Error sending webhook message');
+    });
+  });
