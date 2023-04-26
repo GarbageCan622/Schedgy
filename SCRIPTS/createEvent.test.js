5d21096300bcc657f3e749c655bdac2992396520
Jayendra Rathore <71295474+JayRathore02@users.noreply.github.com>
2023-04-25 22:53:09 -0400

    Update createEvent.test.js

--- SCRIPTS/createEvent.test.js
+++ SCRIPTS/createEvent.test.js
@@ -1,72 +1,88 @@
+const createEvent = require('./createEvent');
+
 describe('createEvent', () => {
-    beforeEach(() => {
-      document.body.innerHTML = `
-        <form id="EventForm">
-          <input id="NewEventName" type="text" value="Test Event" />
-          <select id="NoEarlierThan">
-            <option value="0">12:00am</option>
-            <option value="1">1:00am</option>
-            <option value="2">2:00am</option>
-          </select>
-          <select id="NoLaterThan">
-            <option value="22">10:00pm</option>
-            <option value="23">11:00pm</option>
-          </select>
-          <select id="DateTypes">
-            <option value="SpecificDates">Specific Dates</option>
-            <option value="DaysOfTheWeek">Days of the Week</option>
-          </select>
-          <div id="SpecificDates">
-            <input type="text" name="daterange" value="03/17/2023 - 03/23/2023" />
-          </div>
-          <div id="DaysOfTheWeek">
-            <input type="checkbox" name="days" value="monday" /> Monday
-            <input type="checkbox" name="days" value="tuesday" /> Tuesday
-            <input type="checkbox" name="days" value="wednesday" /> Wednesday
-            <input type="checkbox" name="days" value="thursday" /> Thursday
-            <input type="checkbox" name="days" value="friday" /> Friday
-            <input type="checkbox" name="days" value="saturday" /> Saturday
-            <input type="checkbox" name="days" value="sunday" /> Sunday
-          </div>
-          <input id="event_id" type="hidden" value="1" />
-          <button type="submit" id="submitBtn">Submit</button>
-        </form>
-      `;
-    });
-  
-    test('selecting Specific Dates shows the correct div', () => {
-      const specificDatesDiv = document.getElementById('SpecificDates');
-      const daysOfWeekDiv = document.getElementById('DaysOfTheWeek');
-      const dateTypesSelect = document.getElementById('DateTypes');
-  
-      dateTypesSelect.value = 'SpecificDates';
-      dateTypesSelect.dispatchEvent(new Event('change'));
-  
-      expect(specificDatesDiv.style.display).toBe('block');
-      expect(daysOfWeekDiv.style.display).toBe('none');
-    });
-  
-    test('selecting Days of the Week shows the correct div', () => {
-      const specificDatesDiv = document.getElementById('SpecificDates');
-      const daysOfWeekDiv = document.getElementById('DaysOfTheWeek');
-      const dateTypesSelect = document.getElementById('DateTypes');
-  
-      dateTypesSelect.value = 'DaysOfTheWeek';
-      dateTypesSelect.dispatchEvent(new Event('change'));
-  
-      expect(specificDatesDiv.style.display).toBe('none');
-      expect(daysOfWeekDiv.style.display).toBe('block');
-    });
-  
-    test('selecting a No Earlier Than time disables appropriate No Later Than options', () => {
-      const noLaterThanSelect = document.getElementById('NoLaterThan');
-      const noEarlierThanSelect = document.getElementById('NoEarlierThan');
-  
-      noEarlierThanSelect.value = '1';
-      noEarlierThanSelect.dispatchEvent(new Event('change'));
-  
-      expect(noLaterThanSelect.children[0].disabled).toBe(true);
-      expect(noLaterThanSelect.children[1].disabled).toBe(false);
-      expect(noLaterThanSelect.children[2].disabled).toBe(false);
-    });
+  // Mock the document object
+  const doc = {
+    readyState: 'complete',
+    addEventListener: jest.fn(),
+    createElement: jest.fn(),
+    getElementById: jest.fn(),
+  };
+
+  beforeEach(() => {
+    // Reset the mock implementations before each test
+    doc.addEventListener.mockReset();
+    doc.createElement.mockReset();
+    doc.getElementById.mockReset();
+  });
+
+  test('it should initialize the date range picker', () => {
+    // Mock the daterangepicker library
+    const daterangepicker = {
+      callCount: 0,
+      mockImplementation: function() {
+        this.callCount++;
+      },
+    };
+
+    // Call the function with the mocked objects
+    createEvent(doc, daterangepicker);
+
+    // Assert that the daterangepicker library was called
+    expect(daterangepicker.callCount).toBe(1);
+  });
+
+  test('it should disable hours later than 9 PM', () => {
+    // Call the function with the mocked objects
+    createEvent(doc);
+
+    // Assert that the options were disabled
+    expect(doc.getElementById).toHaveBeenCalledTimes(2);
+    expect(doc.getElementById).toHaveBeenCalledWith('NoLaterThan');
+    expect(doc.getElementById).toHaveBeenCalledWith('NoEarlierThan');
+
+    const noLaterThan = doc.getElementById('NoLaterThan');
+    const noEarlierThan = doc.getElementById('NoEarlierThan');
+
+    for (let i = 10; i < 24; i++) {
+      expect(noLaterThan.options[i].disabled).toBe(true);
+      expect(noEarlierThan.options[i].disabled).toBe(false);
+    }
+  });
+
+  test('it should disable hours earlier than 5 PM', () => {
+    // Call the function with the mocked objects
+    createEvent(doc);
+
+    // Assert that the options were disabled
+    expect(doc.getElementById).toHaveBeenCalledTimes(2);
+    expect(doc.getElementById).toHaveBeenCalledWith('NoLaterThan');
+    expect(doc.getElementById).toHaveBeenCalledWith('NoEarlierThan');
+
+    const noLaterThan = doc.getElementById('NoLaterThan');
+    const noEarlierThan = doc.getElementById('NoEarlierThan');
+
+    for (let i = 0; i < 17; i++) {
+      expect(noLaterThan.options[i].disabled).toBe(false);
+      expect(noEarlierThan.options[i].disabled).toBe(true);
+    }
+  });
+
+  test('it should show the specific dates input when selected', () => {
+    // Call the function with the mocked objects
+    createEvent(doc);
+
+    // Assert that the elements were hidden by default
+    expect(doc.getElementById('SpecificDates').style.display).toBe('none');
+    expect(doc.getElementById('DaysOfTheWeek').style.display).toBe('block');
+
+    // Select the SpecificDates option
+    const dateTypes = doc.getElementById('DateTypes');
+    dateTypes.value = 'SpecificDates';
+    dateTypes.dispatchEvent(new Event('change'));
+
+    // Assert that the SpecificDates input is visible
+    expect(doc.getElementById('SpecificDates').style.display).toBe('block');
+    expect(doc.getElementById('DaysOfTheWeek').style.display).toBe('none');
+  });
 });
